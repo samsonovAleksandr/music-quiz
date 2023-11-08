3b8c25285c1963faaef5314d13fa6da92d36d01f
Александр Фёдорович Самсонов <115645949+samsonovAleksandr@users.noreply.github.com>
2023-11-08 10:11:31 +0500

    add добавлен метод выборки 5 случайных строчек из песни

--- src/main/java/com/example/musicquix/service/MusicService.java
+++ src/main/java/com/example/musicquix/service/MusicService.java
@@ -8,13 +8,14 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class MusicService {
 
     private final MusicRepository musicRepository;
-   private final MusicMapper musicMapper;
+    private final MusicMapper musicMapper;
 
     public MusicService(MusicRepository musicRepository, MusicMapper musicMapper) {
         this.musicRepository = musicRepository;
@@ -26,7 +27,22 @@ public class MusicService {
         for (int i = 0; i <= 3; i++) {
             long randomId = ThreadLocalRandom.current().nextLong(150L);
             musics.add(musicRepository.getReferenceById(randomId));
+            musics.get(i).setLyrics(textSongSplit(musics.get(i).getLyrics()));
+
         }
         return musicMapper.requestMusic(musics);
+    }
+
+    private String textSongSplit(String text) {
+        String finalText = "";
+        String[] textSong = text.split("\n");
+        Random random = new Random();
+        int index = random.nextInt(textSong.length);
+        if (index <= 5) index = 5;
+        if (index >= (textSong.length - 5)) index = textSong.length - 5;
+        for (int i = 0; i <= 4; i++) {
+            finalText += textSong[index + i] + "\n";
+        }
+        return finalText;
     }
 }
